b9f46bba1d794742aa4de8b97ae799f302c0c129
Plus 3 <pankajs@plusonesoftwares.com>
2017-01-25 17:02:59 +0530

    delete

--- app/src/main/java/com/example/plus3/chatme/UserList.java
+++ app/src/main/java/com/example/plus3/chatme/UserList.java
@@ -1,9 +1,6 @@
 package com.example.plus3.chatme;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -22,12 +19,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -95,6 +86,7 @@ public class UserList extends AppCompatActivity {
                         if(dataSnapshot2.getKey().equals("Name") && !dataSnapshot2.getValue().equals(USER_NAME)){
                             list.add(dataSnapshot2.getValue().toString());
                         }
+
                        adapter.notifyDataSetChanged();
                    }
                  }
@@ -141,39 +133,6 @@ public class UserList extends AppCompatActivity {
 
             }
         });
-
-    }
-
-    //Class for image download
-    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
-
-        @Override
-        protected Bitmap doInBackground(String... urls) {
-
-            try {
-                URL url = new URL(urls[0]);
-                URLConnection urlConnection = url.openConnection();
-                HttpURLConnection httpURLConnection = null;
-
-                if(urlConnection instanceof HttpURLConnection){
-                    httpURLConnection = (HttpURLConnection) urlConnection;
-                    httpURLConnection.connect();
-                    InputStream inputStream = httpURLConnection.getInputStream();
-
-                    Bitmap myImage = BitmapFactory.decodeStream(inputStream);
-                    return myImage;
-                }
-            }
-            catch (MalformedURLException e) {
-                e.printStackTrace();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            return null;
-        }
     }
 
     @Override
