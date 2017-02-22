b411e4deea46e0549fe8a3d31ba8fc2bd333c712
ashoksharma <User@123456>
2017-02-22 13:21:27 +0530

    1. Back from main tab view should close the app.

--- app/src/main/java/com/example/plus3/chatme/TabViewActivity.java
+++ app/src/main/java/com/example/plus3/chatme/TabViewActivity.java
@@ -134,7 +134,7 @@ public class TabViewActivity extends AppCompatActivity implements TabLayout.OnTa
     public void onBackPressed() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+        intent.setFlags(Intent.FLAG_FROM_BACKGROUND);//***Change Here***
         startActivity(intent);
         finish();
         System.exit(0);
