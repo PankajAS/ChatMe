87f18a7fe40727897aeeadba5bab3320461c7240
ashoksharma <User@123456>
2017-02-23 22:53:58 +0530

    solved the crash problem and updated the chat screen ui.

--- app/src/main/java/com/example/plus3/chatme/SplashScreen.java
+++ app/src/main/java/com/example/plus3/chatme/SplashScreen.java
@@ -3,18 +3,22 @@ package com.example.plus3.chatme;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import static android.content.ContentValues.TAG;
 
 public class SplashScreen extends Activity {
 
@@ -23,27 +27,44 @@ public class SplashScreen extends Activity {
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     private String userId;
-
+    static boolean isInitialized = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);//Enable offline storage for application
 
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Users");
-
-        if(user!=null && user.getUid()!=null){
-            Intent intent = new Intent(getApplicationContext(),TabViewActivity.class);
-            userId = user.getUid().toString();
-            intent.putExtra("UID",userId);
-            startActivity(intent);
+        try {
+            if(!isInitialized){
+                FirebaseDatabase.getInstance().setPersistenceEnabled(true);//Enable offline storage for application
+                isInitialized = true;
+            }else {
+                Log.d(TAG,"Already Initialized");
+                //Toast.makeText(getApplicationContext(), ex.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        else
+
+        try {
+            auth = FirebaseAuth.getInstance();
+            user = auth.getCurrentUser();
+            database = FirebaseDatabase.getInstance();
+            myRef = database.getReference("Users");
+
+            if (user != null && user.getUid() != null) {
+                Intent intent = new Intent(getApplicationContext(), TabViewActivity.class);
+                userId = user.getUid().toString();
+                intent.putExtra("UID", userId);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+        catch(Exception ex)
         {
+            Toast.makeText(getApplicationContext(), ex.getMessage(),Toast.LENGTH_LONG).show();
             Intent intent = new Intent(SplashScreen.this, MainActivity.class);
             startActivity(intent);
         }
